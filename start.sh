779688753326be9f9daf5187ca6c4659ae3a922d
Johan Lundberg <lundberg@sunet.se>
2020-07-14 13:52:36 +0200

    Use start-stop-daemon and tail log

--- start.sh
+++ start.sh
@@ -5,6 +5,12 @@ cp -r ${DATA_DIR:?"Need to set DATA_DIR non-empty"}/* .
 
 # generate SAML metadata
 make_metadata.py sp_conf.py > sp.xml
+cp sp.xml ${DATA_DIR}/.
 
 # start the Service Provider
-exec python3 sp.py sp_conf $*
+echo "Starting pysaml2 SP..."
+tail -F /tmp/src/pysaml2/example/sp-wsgi/spx.log &
+exec start-stop-daemon --start --exec \
+    /usr/bin/python3 \
+    --chdir /tmp/src/pysaml2/example/sp-wsgi/ -- \
+    sp.py sp_conf $*
